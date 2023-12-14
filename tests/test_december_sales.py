b50d4583ffc9295f2404738bc17bc8630bd250ef
thelaycon <tobitobitobiwhy@gmail.com>
2023-12-14 07:30:51 +0100

    reverted due to cloudflare

--- tests/test_december_sales.py
+++ tests/test_december_sales.py
@@ -22,6 +22,7 @@ def homepage(page: Page):
 @when("the december offer is clicked")
 def december_offer_clicked(page: Page):
     home = Homepage(page)
+    expect(home.christmas_products()).to_be_visible()
     home.click_december_sales()
 
 
